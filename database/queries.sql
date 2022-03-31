726ff6d87cfa2cb9660ad9f17c85000851c8d0d9
DiegoPerdiguero <rokhsa@gmail.com>
2022-03-31 16:50:19 +0200

    31 de marzo

---
+++ database/queries.sql
@@ -0,0 +1,36 @@
+
+/*RESULTADOS POR FAMILIAS */
+SELECT * 
+FROM Artemisa_Preguntas
+WHERE pregunta_familia LIKE 'mamíferos';
+
+SELECT * 
+FROM Artemisa_Preguntas
+WHERE pregunta_familia LIKE 'aves';
+
+SELECT * 
+FROM Artemisa_Preguntas
+WHERE pregunta_familia LIKE 'reptiles';
+
+SELECT * 
+FROM Artemisa_Preguntas
+WHERE pregunta_familia LIKE 'anfibios';
+
+SELECT * 
+FROM Artemisa_Preguntas
+WHERE pregunta_familia LIKE 'peces';
+
+SELECT * 
+FROM Artemisa_Preguntas
+WHERE pregunta_familia LIKE 'insectos';
+
+
+/* RESULTADOS POR BÚSQUEDA */
+
+SELECT * 
+FROM Artemisa_Preguntas
+WHERE pregunta_nombreComun LIKE "%%";
+
+SELECT * 
+FROM Artemisa_Preguntas
+WHERE pregunta_nombreCientifico LIKE "%%";
